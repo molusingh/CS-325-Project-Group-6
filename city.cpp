4678671e248a9ddc7fbd291f83c474fa80020dbb
M-Krilano <35286146+M-Krilano@users.noreply.github.com>
2018-06-03 19:52:48 -0700

    updated

--- city.cpp
+++ city.cpp
@@ -2,26 +2,42 @@
 
 
 city::city() {
+	this->city_id = 0;
+	this->x_coord = 0;
+	this->y_coord = 0;
 	visited = false;
-};
+}
+city::city(int input_id, int input_x, int input_y) {
+	this->city_id = input_id;
+	this->x_coord = input_x;
+	this->y_coord = input_y;
+	this->visited = false;
+}
 
 city::~city() {
-};
-
-bool city::isCandidate() {
-
-};
-
-int city::getID() {
-	return id;
 }
-int city::getX() {
-	return x;
+
+void city::set_city_id(int num){
+	this->city_id = num;
 }
-int city::getY() {
-	return y;
+void city::set_x_coord(int num) {
+	this->x_coord = num;
 }
-int city::distance(city* input_city)
-{
-	int 
+void city::set_y_coord(int num) {
+	this->y_coord = num;
+}
+void city::set_visited(bool val) {
+	this->visited = val;
+}
+int city::get_city_id() {
+	return this->city_id;
+}
+int city::get_x_coord() {
+	return this->x_coord;
+}
+int city::get_y_coord() {
+	return this->y_coord;
+}
+bool city::get_visited() {
+	return this->visited;
 }
