6ccc827f67662ce5ace2c0e1ac1904f67aea3eec
M-Krilano <35286146+M-Krilano@users.noreply.github.com>
2018-06-03 19:54:19 -0700

    updated

--- main.cpp
+++ main.cpp
@@ -8,17 +8,42 @@ int main() {
 
 	// open the input file
 	ifstream input_file("data.input");
+	// open the output file
+	ofstream output_file("data.output"); 
 
 	// check for successful opening
 	if (!input_file.is_open()) {
-		cout << "Error: File could not be opened." << endl;
+		cout << "Error: Input File could not be opened." << endl;
+		return 1;
 	}
+	// check for successful opening
+	if (!output_file.is_open()) { 
+		cout << "Error: Output file could not be opened." << endl;
+		return 1;
+	}
+	// read as 
 
 	// read file
-	
+	// create vector of cities
+	while (!input_file.eof()) {
+	// read three ints
 
+	// make into city object
+
+	// add to vector
+
+	}
 	// close file
 	input_file.close();
+	
+
+	// create graph map[start][end]
+	// calc distance (weight) between two cities
+	// call 2-opt
+	//output 
+
+	// close file
+	output_file.close();
 
 	return 0;
 }
