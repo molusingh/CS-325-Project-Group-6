d1f703e9a9e247843e3622d1f9ea00b699dbaccb
Anmolbir Mann <mannanm@flip1.engr.oregonstate.edu>
2018-06-08 13:31:39 -0700

    Added files used to get results

---
+++ getDistanceResults.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+echo "example1"
+check.py example1.txt
+echo "example2"
+check.py example2.txt
+echo "example3"
+check.py example3.txt
+
+echo "test1"
+check.py test1.txt
+echo "test2"
+check.py test2.txt
+echo "test3"
+check.py test3.txt
+echo "test4"
+check.py test4.txt
+echo "test5"
+check.py test5.txt
+echo "test6"
+check.py test6.txt
+echo "test7"
+check.py test7.txt
---
+++ getResults.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+make
+
+echo "example1" 
+2opt example1.txt  
+echo "example2" 
+2opt example2.txt  
+echo "example3"  
+2opt example3.txt  
+
+echo "test1" 
+2opt test1.txt   
+echo "test2" 
+2opt test2.txt  
+echo "test3" 
+2opt test3.txt  
+echo "test4" 
+2opt test4.txt   
+echo "test5" 
+2opt test5.txt   
+echo "test6" 
+2opt test6.txt   
+echo "test7" 
+2opt test7.txt
+   
